7610d5f9c3a3a2d0a39e98849aba4f811fb7cfd4
huntersaddler <91514859+huntersaddler@users.noreply.github.com>
2021-11-12 23:47:41 -0600

    Add files via upload

--- QR_saddlerHunter.py
+++ QR_saddlerHunter.py
@@ -55,3 +55,136 @@ def orthonormal_basis(matrix: list) ->list:
     """
     H: list = stable_gramschmidt(matrix)
     return H[0]
+
+
+def deep_copy(matrix: list) -> list:
+    """Outputs a copy of the input matrix that can be changed without changing the original
+
+        Creates a empty list V then for each col in the input matrix we append each col to V
+
+        Args:
+             matrix is a list of column vectors
+        Returns:
+            V, a deep copy of matrix
+    """
+    V: list = []
+    for index in range(len(matrix)):
+        V.append(matrix[index])
+    return V
+
+
+def ID_build(size: int) -> list:
+    """Creates an identity matrix with the given size
+
+        Makes empty list I and c, then for each col we check if the row is equal to the col. If equal, we append 1 to c.
+        else we append 0 to c. Then we append each col stored in c to I. returns I
+
+        Args:
+            size, the desired size of the id matrix
+        Returns:
+            I, the identity matrix
+    """
+    I: list = []
+    for col in range(size):
+        c: list = []
+        for row in range(size):
+            if row == col:
+                c.append(1)
+            else:
+                c.append(0)
+        I.append(c)
+    return I
+
+
+def sub_col(matrix: list, col: int) -> list:
+    """Takes the sub col of the given matrix along the diagonol for the given col
+
+        Creates a vector B and stores the desired col. Then deletes the numbers above until we have the desired subcol
+        along the diaganol of the matrix
+
+        Args:
+            matrix, a list of col vectors
+            col, the desired col to take the sub col
+        Returns:
+            B, the sub col along the diaganol of the input matrix
+    """
+    B = (matrix[(col-1)])
+    for int in range(col-1):
+        del B[int]
+    return B
+
+
+def ele_conj(element: complex) -> complex:
+    """Conjugates the elemennt given. For some reason only was working when i made it a function
+
+        Simply returns the conjugate of an element.
+
+        Args:
+            element, the element you want to conjugate
+        Returns:
+            the conjugate of the element given
+    """
+    return element.conjugate()
+
+
+def conjugate_transpose(matrix: list) -> list:
+    """Takes the conjugate transpose of a matrix
+
+        Creates an empty list V then for each col in the given matrix we append the element at the given row to r. Then we
+        append r to V. Returns V
+
+        Args:
+            matrix, a list of col vectors
+        Returns:
+            V the conjugate transpose of the given matrix
+    """
+    V: list = []
+    for col in range(len(matrix[0])):
+        r = []
+        for row in range(len(matrix)):
+            r.append(ele_conj(matrix[row][col]))
+        V.append(r)
+    return V
+
+
+def f_builder(V: list) -> list:
+    """Creates the F matrix for the housholder eq to use in computing Q
+
+        Computes the transpse and inner product of V the input vector, then finds 2 * Vtranspose * V and stores as Vmulti. Then finds the difference
+        between I and the negated Vmulti. Returns F
+
+        Args:
+            V, a col vector
+        Returns:
+            F, the result of the formula in the householder equation
+    """
+    transposeV: list = conjugate_transpose(V)
+    innerV: complex = LA_saddlerHunter.matrix_matrix_mult(transposeV, V)[0][0]
+    Vmulti: list = LA_saddlerHunter.scal_matrix_mult((2/innerV), LA_saddlerHunter.matrix_matrix_mult(V, transposeV))
+    nVmulti: list = (LA_saddlerHunter.scal_matrix_mult(-1, Vmulti))
+    F = LA_saddlerHunter.matrix_matrix_add(ID_build(len(Vmulti)), nVmulti)
+    return F
+
+"""Unfortunately i have broken my f builder due to an issue with how I wrote my norm functions in my LA script.
+    I believe all the support functions are working correctly but i have run out of time
+   
+   REMINDER to finish this weekend or be in office hours on monday
+"""
+
+
+def q_builder(matrix: list, k: int):
+    """Builds the Q vector for a given F and k the col number
+
+        Creates and Id matrix +1 the size of the F matrix given. Then for each col in Q we check if the row and col is the same
+        as k. If it is we replace that index with the corresponding index in the input matrix until we have built Q. Returns Q
+
+        Args:
+            matrix, the F matrix from the householder eq stored as list of col
+            k, the col where the F matrix needs to be in Q
+        Returns:
+            Q, the matrix of col vectors built by F
+    """
+
+def householder(matrix: list):
+    """Finds the QR orth of the given matrix and returns [Q, R]
+     """
--- QR_testfile_saddlerHunter.py
+++ QR_testfile_saddlerHunter.py
@@ -15,3 +15,28 @@ assert QR_saddlerHunter.stable_gramschmidt(qrtest1) == [[[(1/(2**(1/2))), 1/(2**
 
 assert QR_saddlerHunter.orthonormal_basis(qrtest2) == [[2/3, 2/3, 1/3], [-1/3, 2/3, -2/3]]
 assert QR_saddlerHunter.orthonormal_basis(qrtest1) == [[(1/(2**(1/2))), 1/(2**(1/2)), 0],[1/(6**(1/2)), -1/(6**(1/2)), 2/(6**(1/2))], [-1/(3**(1/2)), 1/(3**(1/2)), 1/(3**(1/2))]]
+
+
+
+
+"""Test for functions for HW7"""
+
+testm_a: list = [[1, 2], [3, 4]]
+testm_b: list = [[5, -1, 0], [2, 4, 3]]
+#test for deep copy creator
+assert QR_saddlerHunter.deep_copy(testm_a) == [[1, 2], [3, 4]]
+assert QR_saddlerHunter.deep_copy(testm_b) == [[5, -1, 0], [2, 4, 3]]
+#test for Identity builder
+assert QR_saddlerHunter.ID_build(2) == [[1, 0], [0, 1]]
+assert QR_saddlerHunter.ID_build(3) == [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+#test for sub column generator
+assert QR_saddlerHunter.sub_col(testm_a, 2) == [4]
+assert QR_saddlerHunter.sub_col(testm_a, 1) == [1, 2]
+#test for element conjugator
+assert QR_saddlerHunter.ele_conj(2) == 2
+assert QR_saddlerHunter.ele_conj(1+2j) == 1-2j
+#test for conjugate transpose
+assert QR_saddlerHunter.conjugate_transpose(testm_a) == [[1, 3], [2, 4]]
+assert QR_saddlerHunter.conjugate_transpose(testm_b) == [[5, 2], [-1, 4], [0, 3]]
+
+
